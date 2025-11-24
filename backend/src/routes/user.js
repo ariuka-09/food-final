6985702b8a118e7d92e2860a6d29f4f97124eaf8
ariuka-09 <ariuntuguldur3@gmail.com>
2025-11-24 15:10:59 +0800

    made changes to backend

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { getUserById, getUsers, logIn, signUp, updateUser } from "../controller/user.js";
+import {
+  getUserById,
+  getUsers,
+  logIn,
+  signUp,
+  updateUser,
+} from "../controller/user.js";
 import { verifyToken } from "../middleware/auth.js";
 
 export const userRouter = Router();
 userRouter
-.post('/signUp', signUp)
-.get('/', getUsers)
-.post('/logIn',logIn )
-.patch('/:id', updateUser)
-.get('/:id',verifyToken, getUserById )
+  .post("/signUp", signUp)
+  .get("/", getUsers)
+  .post("/signIn", logIn)
+  .patch("/:id", updateUser)
+  .get("/:id", verifyToken, getUserById);
